93a70f5361e5ed170328508de411c9f0c59ec4fe
pyj <pyj2qdat@naver.com>
2025-06-27 14:20:49 +0900

    day16

--- pages/day16.py
+++ pages/day16.py
@@ -1,17 +1,17 @@
 import streamlit as st
-st.title("Customizing the theme of Streamlit apps")
-st.write('이 앱의 `.strealit/config.toml`파일 내용')
-st.code('''
-        [theme]
-        primaryColor='#A5AEFF'
-        backgroundColor='#fff0d9'
-        secondaryBackgroundColor="#FFDDEF"
-        textColor="#FFFFFF"
-        font="monospace"
-""")
-        
-number = st.sidebar.slider('숫자를 선택하세요:',0,10,5)
-st.write('슬라이더 위젯에서 선택된 숫자:',number)
-        
 
-        
+st.title('Streamlit 앱의 테마 사용자 정의하기')
+
+st.write('이 앱의 `.streamlit/config.toml` 파일 내용')
+
+st.code("""
+[theme]
+primaryColor="#F39C12"
+backgroundColor="#2E86C1"
+secondaryBackgroundColor="#AED6F1"
+textColor="#FFFFFF"
+font="monospace"
+""")
+
+number = st.sidebar.slider('숫자를 선택하세요:', 0, 10, 5)
+st.write('슬라이더 위젯에서 선택된 숫자:', number)
